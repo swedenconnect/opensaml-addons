620b60e950e6b1be9e374849d82848d4583878a8
Martin Lindström <martin.lindstrom@litsec.se>
2025-05-08 12:12:46 +0200

    2.1.5

--- src/main/java/se/swedenconnect/opensaml/common/LibraryVersion.java
+++ src/main/java/se/swedenconnect/opensaml/common/LibraryVersion.java
@@ -24,7 +24,7 @@ public final class LibraryVersion {
 
   private static final int MAJOR = 2;
   private static final int MINOR = 1;
-  private static final int PATCH = 4;
+  private static final int PATCH = 5;
 
   /**
    * Global serialization value for library classes.
