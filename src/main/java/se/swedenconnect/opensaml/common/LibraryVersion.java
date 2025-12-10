4110a2a427e413ecfbe319c036547a8565179c04
Martin Lindström <martin-lindstrom@users.noreply.github.com>
2025-12-10 15:33:07 +0100

    IS-91 Fixed NPE bug when assertion is not set (#92)

--- src/main/java/se/swedenconnect/opensaml/common/LibraryVersion.java
+++ src/main/java/se/swedenconnect/opensaml/common/LibraryVersion.java
@@ -24,7 +24,7 @@ public final class LibraryVersion {
 
   private static final int MAJOR = 2;
   private static final int MINOR = 1;
-  private static final int PATCH = 7;
+  private static final int PATCH = 8;
 
   /**
    * Global serialization value for library classes.
